f8e8ca05285f1cb0d92e005e54b8e401663e7b68
gabrielrc11 <gabrielhenriquerc11@gmail.com>
2025-05-03 23:50:34 -0300

    feat: Ler CSV

--- exports_data.py
+++ exports_data.py
@@ -10,3 +10,18 @@ DB_CONFIG = {
     'user': 'postgres',
     'password': 'admin'
 }
+
+# Função para ler o arquivo CSV
+def read_csv(file_path):
+    csv_file = file_path
+    
+    # Lendo o CSV com pandas
+    df = pd.read_csv(csv_file, sep=';', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    
+    # Convertendo colunas numéricas para tipos apropriados
+    numeric_cols = ['CO_ANO', 'CO_MES', 'QT_ESTAT', 'KG_LIQUIDO', 'VL_FOB']
+    for col in numeric_cols:
+        df[col] = pd.to_numeric(df[col], errors='coerce')
+    
+    return df
+
